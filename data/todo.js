fa18c13666381c38750735f914548edf05de4cdf
Matthew Saber <mattsaber21@yahoo.com>
2021-05-13 13:40:53 -0700

    updated datafile

---
+++ data/todo.js
@@ -0,0 +1,69 @@
+// example seed data
+const todo = [
+  //   {
+  //     id: 3,
+  //     task: 'wash the dishes',
+  //     completed: false,
+  //     shared: false, // STRETCH
+  //     userId: 4,
+  //     userName: 'Chris' // STRETCH: only on GET all public TODO
+  // }
+  {
+    id: 1,
+    task: 'Catch \'em all',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  },
+  {
+    id: 0,
+    task: '',
+    completed: false,
+    //shared:,
+    userId: 0,
+    userName:
+  }
+];
+
+export default todo;
